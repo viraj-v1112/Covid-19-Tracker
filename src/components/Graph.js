936ec2044436ad52c293f566da9e7223f7860884
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2020-04-05 13:40:07 +0530

    Graph Updated

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -5,7 +5,7 @@ import {
   CartesianGrid,
   XAxis,
   YAxis,
-  Tooltip
+  Tooltip,
 } from "recharts";
 import CoronaContext from "../context/corona/coronaContext";
 import Spinner from "./Spinner";
@@ -21,12 +21,12 @@ const Graph = () => {
     // eslint-disable-next-line
   }, []);
   if (history.length > 0) {
-    data = history.map(historydata => ({
+    data = history.map((historydata) => ({
       name: historydata.day,
       Total: historydata.total.confirmed,
       Recovered: historydata.total.recovered,
       Deaths: historydata.total.deaths,
-      Active: historydata.total.active
+      Active: historydata.total.active,
     }));
     console.log(history);
     //console.log(day);
@@ -42,14 +42,34 @@ const Graph = () => {
         width={600}
         height={300}
         data={data}
-        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        margin={{ top: 5, right: 20, bottom: 20, left: 0 }}
       >
-        <Line type='monotone' dataKey='Total' stroke='#004e92' />
-        <Line type='monotone' dataKey='Active' stroke='#7b4397' />
-        <Line type='monotone' dataKey='Recovered' stroke='#0f9b0f' />
-        <Line type='monotone' dataKey='Deaths' stroke='#6f0000' />
+        <Line
+          type='monotone'
+          dataKey='Total'
+          stroke='#004e92'
+          activeDot={{ r: 6 }}
+        />
+        <Line
+          type='monotone'
+          dataKey='Active'
+          stroke='#7b4397'
+          activeDot={{ r: 6 }}
+        />
+        <Line
+          type='monotone'
+          dataKey='Recovered'
+          stroke='#0f9b0f'
+          activeDot={{ r: 6 }}
+        />
+        <Line
+          type='monotone'
+          dataKey='Deaths'
+          stroke='#6f0000'
+          activeDot={{ r: 6 }}
+        />
         <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-        <XAxis dataKey='name' />
+        <XAxis dataKey='name' interval={3} />
         <YAxis />
         <Tooltip />
       </LineChart>
